8f50ca0564d16546f90cd25f726a6ec63b879755
Cesar Severo <cesar.pires.severo@gmail.com>
2021-08-31 22:27:37 +0900

    Adding sort search

--- Encyclopedia/dsp_encyclopedia.py
+++ Encyclopedia/dsp_encyclopedia.py
@@ -4,9 +4,6 @@
 #                                                                        #
 ##########################################################################
 
-#The YAML file to source from
-filename="example.yaml"
-
 '''
 *	Overview
    Easy way to seach and collect information about pertinent DSP 
@@ -22,12 +19,12 @@ filename="example.yaml"
    resources are there.
    The library application will be entire accesed with the terminal and
    won't use any GUI for usage.
+
 *	Features
    The search engine will sort subjects automatically by:
    - Alphabetical order
    - Date modified
-   - Category alphabetical order -> alphabetical order
-   - Category alphabetical order -> date modified
+   - Category alphabetical order
    The user can make custom search by:
    - Letters: search any ammount of letters that matches title + contents
    - Keywords: search in title + contents
@@ -35,34 +32,150 @@ filename="example.yaml"
    The user can also add aditional chapters inside each subject that will 
    be used as index. The user than can access them individually otherwise
    their contents will be omitted when the main subject is accessed.
+
 *	Data structure
    The data structure used will be YAML due to its similarity to Python
    indexation use. Here is an example of usage:
 	---
+   #Subject name
 	Polyphase quadrature filter:
-   	category: ["Filters","FIR"]
-   	date: "12/12/2021"
-   	links: "https://en.wikipedia.org/wiki/Polyphase_quadrature_filter"
+      #List or single string          
+   	category: ["Filters","FIR"]   
+      #YYYY/MM/DD   
+   	date: "2021/05/04"
+      #List or single string
+   	links: "https://en.wikipedia.org/wiki/Polyphase_quadrature_filter" 
+      # |- added to keep the newlines
    	contents: |-
-         A polyphase quadrature filter, or PQF, is a filter bank which splits an input signal into a given number N (mostly a power of 2) of equidistant sub-bands. 
-         These sub-bands are subsampled by a factor of N, so they are critically sampled.
-         An important application of the polyphase filters (of FIR type) concerns the filtering and decimation of large band (and so high sample rate) input signals, e.g. coming from a high rate ADC, which can not be directly processed by an FPGA or in some case by an ASIC either.
-
-   	
+         A polyphase quadrature filter, or PQF, is a filter bank which 
+         splits an input signal into a given number N (mostly a 
+         power of 2) of equidistant sub-bands. 
+      # Can have any size of components
+      subcontents:
+         Applications: Compression, decimation and interpolation
+         Comercial Usage: MP3	
 	---
+   General rules:
+   - The names "category","date","links","contents" and "subcontents" 
+   are mandatory and other names will be ignored
+   - No field is mandatory for the code to work. Non complete fields will 
+   be set as "Null" in the search
+   - Only contents will be displayed, the other fields will be used as
+   metadata unless user requestes more info
+
 *	Future ideas
+   - Add pictures from "assets" automatically showing in the terminal or
+   externally as set by the user
+   - Add MSDOS aesthetic to the tool 
 
 '''
 
-import yaml
+#Texts used in the application
+intro_text='''
+################################################################################
+#                                                                              #
+#                       DSP Interactive Encyclopedia                           #
+#                                                                              #
+################################################################################
+   
+   Easy way to seach and collect information about pertinent DSP algorithms. 
+   This content will be read from a source file that the data can be added in 
+   any order. The idea is to summerize and create an encyclopedia with all 
+   important DSP concepts in one organized place that can be referenced easier 
+   than a single book or a online search. 
+'''
+options_text='''
+----------------------------------------   -------------------   ----------
+| Sort by (N)ame, (D)ate or (C)ategory |   | Custom (S)earch |   | (E)xit |
+----------------------------------------   -------------------   ----------
+'''
+searc_text='''
+----------------   ------------   ----------
+| (N)ew search |   | (R)eturn |   | (E)xit |
+----------------   ------------   ----------
+'''
+return_text='''
+------------   ----------
+| (R)eturn |   | (E)xit |
+------------   ----------
+'''
 
-with open(filename, 'r') as input_data:
-   try:
-         data=yaml.safe_load(input_data)
-         subjects=[]
-         subjects=list(data.keys())
-         print(data[subjects[1]]["contents"])
-         print(subjects)
-        
-   except yaml.YAMLError as exc:
-      print(exc)
+#Importing libs
+import os
+import yaml
+import time
+
+#The YAML file to source from
+filename="example.yaml"
+
+#The application class
+class application:
+   def __init__(self):
+      self.clear_screen()
+      self.data={} #YAML file dictionary
+      self.status=0 #0-> Main menu, 1-> Sort menu, 2-> Search Menu, 3-> Subject menu, 4-> Subcontent menu
+      self.read_file()
+      self.menus()
+
+   def clear_screen(self):
+      os.system('cls' if os.name == 'nt' else 'clear')  
+
+   def read_file(self):
+      with open(filename, 'r') as input_data:
+         try:
+            self.data=yaml.safe_load(input_data)  
+         except yaml.YAMLError as exc:
+            print("Error reading the YAML file:"+filename)
+            raise
+   def print_main_screen(self):
+      self.clear_screen()
+      print(intro_text,options_text)
+
+   def get_user_input(self):
+      return(input())
+
+   def menus(self):
+      while True:
+         if(self.status==0): #Main menu
+            #Showing main screen
+            print(intro_text,options_text)
+            while True:
+               keypress=self.get_user_input()
+               if(keypress=="e"): return
+               if(keypress not in "ndcs") or keypress=="" or len(keypress)>1:
+                  self.print_main_screen()
+               else:
+                  if(keypress in "ndc"): #Sort
+                     self.status=1
+                     break
+                  else:                  #Search
+                     self.status=2
+                     break
+               time.sleep(0.1)
+         elif(self.status==1):
+            subjects=list(self.data.keys())
+            print(self.data[subjects[1]]["contents"])
+            break
+         elif(self.status==2):
+            break
+         elif(self.status==3):
+            break
+         elif(self.status==4):
+            break
+         time.sleep(0.1)
+
+
+if __name__ == '__main__':
+   app_obj=application()
+
+
+
+
+# dictv=dict(data.values())
+# print(dictv["category"])
+# print(data)
+# subjects=[]
+# subjects=list(data.keys())
+# print(app_obj.data[subjects[1]]["contents"])
+# print(app_obj.data[subjects[1]]["subcontents"])
+# print(subjects)
